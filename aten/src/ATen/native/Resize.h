00178f5ae557463b936a32fd940b58ec9d288973
Bob Ren <bobrenjc93@users.noreply.github.com>
2026-03-18 08:05:55 +0000

    Use deferred symbolic checks in unchecked as_strided

--- aten/src/ATen/native/Resize.h
+++ aten/src/ATen/native/Resize.h
@@ -205,14 +205,14 @@ inline void checkAsStridedArgsUnchecked(
   TORCH_CHECK(
       size.size() == stride.size(), "mismatch in length of strides and shape");
   for (const auto& val : stride) {
-    TORCH_CHECK(
-        TORCH_GUARD_OR_TRUE(sym_ge(val, 0)),
+    TORCH_MAYBE_SYM_CHECK(
+        sym_ge(val, 0),
         "as_strided: Negative strides are not supported at the moment, "
         "got strides: ",
         stride);
   }
-  TORCH_CHECK(
-      TORCH_GUARD_OR_TRUE(sym_ge(storage_offset, 0)),
+  TORCH_MAYBE_SYM_CHECK(
+      sym_ge(storage_offset, 0),
       "Tensor: invalid storage offset ",
       storage_offset);
 }
